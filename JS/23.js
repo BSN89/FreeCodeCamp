47f42bf7872100fad75800bc4047e353155f69c3
BSN89 <barakovsn55@gmail.com>
2023-02-04 09:14:37 +0300

    5 feb

---
+++ JS/23.js
@@ -0,0 +1,6 @@
+// Setup
+const lastName = "Lovelace";
+const lastLetter = lastName[lastName.length - 1];
+// Only change code below this line
+const lastLetterOfLastName = lastLetter; // Change this line
+console.log(lastLetterOfLastName);
---
+++ JS/24.js
@@ -0,0 +1,6 @@
+// Setup
+const lastName = "Lovelace";
+const newLast = lastName[lastName.length -2]
+// Only change code below this line
+const secondToLastLetterOfLastName = newLast; // Change this line
+console.log(secondToLastLetterOfLastName)
