d0dc518314d0ed5d0a76b6ea6b8b385b5ee67208
BSN89 <barakovsn55@gmail.com>
2024-11-20 22:23:18 +0300

    20 nov

---
+++ Legacy-JavaScript/reverse-a-string.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
+
+// Перевернуть предоставленную строку и вернуть перевернутую строку.
+
+// Например, "hello"должно стать "olleh".
