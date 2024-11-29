80d109a55d814927d00294c9fe47ecae6c1cd118
BSN89 <barakovsn55@gmail.com>
2024-11-29 09:55:36 +0300

    29 nov

---
+++ Legacy-JavaScript/avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,15 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+let copuFixedValue = fixedValue
+return copuFixedValue + 1
+  // Only change code above this line
+}
+
+// Заполните код функции incrementerтак, чтобы она возвращала значение глобальной переменной, fixedValueувеличенное на единицу.
+
+// Ожидающий :1. Ваша функция incrementerне должна изменять значение fixedValue(которое равно 4).
+// Ожидающий :2. Ваша incrementerфункция должна возвращать значение, на единицу большее указанного fixedValueзначения.
+// Ожидающий :3. Ваша incrementerфункция должна возвращать значение, основанное на fixedValueзначении глобальной переменной.
---
+++ Legacy-JavaScript/pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,17 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(fixedValue) {
+let copyValue = fixedValue
+return copyValue + 1
+  // Only change code above this line
+}
+
+// Давайте обновим incrementerфункцию, чтобы четко обозначить ее зависимости.
+
+// Напишите incrementerфункцию так, чтобы она принимала аргумент, а затем возвращала результат после увеличения значения на единицу.
+
+// 1. Ваша функция incrementerне должна изменять значение fixedValue(которое равно 4).
+// Ожидающий :2. Ваша incrementerфункция должна принимать аргумент.
+// Ожидающий :3. Ваша incrementerфункция должна возвращать значение, на единицу большее указанного fixedValueзначения.
